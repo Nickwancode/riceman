9673f28eb46262362c5fa81df6c72632ea95eea7
Nickwancode <simplynick0904@gmail.com>
2024-03-13 21:55:24 -0400

    added scanner

--- riceman/RiceWorld.java
+++ riceman/RiceWorld.java
@@ -1,13 +1,27 @@
 package riceman;
 
+import java.util.Scanner;
+
 public class RiceWorld {
     public static void main(String[] args) {
-      String productname = "Rice";
-      int qty = 20; // Nick wants 20 rice
-      float price = 5.0f; // Nick know rice is 5 dollars each
-      int miles = -1; // nick live 10 miles away
+       // nick live 10 miles away
       int deliveryFee = 0; // unknown
-      if (miles >=0 && miles <=4) { // Use && to check two conditions at the same time
+
+        Scanner riceWrld = new Scanner(System.in);
+        System.out.println("What item are you seeking today?");
+        String productname = riceWrld.next();
+
+        System.out.println("how many " + productname + " would you want?");
+        int qty = riceWrld.nextInt();
+
+        System.out.println("Okay, whats the price?");
+        float price = riceWrld.nextFloat();
+
+        System.out.println("How far are you");
+        int miles = riceWrld.nextInt();
+
+
+        if (miles >=0 && miles <=4) { // Use && to check two conditions at the same time
           deliveryFee = 2;
       } // if statement conditions return boolean true or false
       else if (miles <=15 && miles >=5) {
@@ -20,8 +34,18 @@ public class RiceWorld {
       else if (miles >50 ) {
           deliveryFee = 20;
       }
-      float totalCost = qty * price + deliveryFee;
+
+        float totalCost = qty * price + deliveryFee;
         System.out.println("Your total will be $" + totalCost);
+
+
+
+
+
+
+
+
+
 
 
 
